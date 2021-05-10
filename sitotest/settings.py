7295b49569448b2e311c481014938e850dec67ce
gitsh1t <esp_ger@yahoo.it>
2021-05-10 14:35:22 +0200

    Update settings.py

--- sitotest/settings.py
+++ sitotest/settings.py
@@ -261,10 +261,10 @@ USE_TZ = True
         # Static files (CSS, JavaScript, Images) #
 # https://docs.djangoproject.com/en/3.0/howto/static-files/ #
 #-----------------------------------------------------------#
-STATIC_ROOT = 'landing_page'
-STATIC_URL = 'landing_page/static/landing_page/'
+STATIC_URL = '/static/landing_page/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR),
+    os.path.join(BASE_DIR, 'landing_page/static/landing_page/'),
 )
 #-----------------------------------------------------------#
 
